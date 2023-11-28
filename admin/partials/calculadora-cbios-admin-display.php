48de073a6c39a274851e9d65aa01b9fc40fe693d
Rafael Bombonatti <rbombonatti@studiovisual.com.br>
2023-11-28 17:24:01 -0300

    Estrutura WPPB

---
+++ admin/partials/calculadora-cbios-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       https://studiovisual.com.br
+ * @since      1.0.0
+ *
+ * @package    Calculadora_Cbios
+ * @subpackage Calculadora_Cbios/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
---
+++ includes/class-calculadora-cbios-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://studiovisual.com.br
+ * @since      1.0.0
+ *
+ * @package    Calculadora_Cbios
+ * @subpackage Calculadora_Cbios/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Calculadora_Cbios
+ * @subpackage Calculadora_Cbios/includes
+ * @author     Studio Visual <contato@studiovisual.com.br>
+ */
+class Calculadora_Cbios_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
