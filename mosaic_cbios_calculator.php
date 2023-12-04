e6e36c0a40b851b2fdf7e1f73bafab57f62f7c46
Rafael Bombonatti <rbombonatti@studiovisual.com.br>
2023-12-04 13:36:22 -0300

    Exclusão arquivos não usados

--- mosaic_cbios_calculator.php
+++ mosaic_cbios_calculator.php
@@ -30,53 +30,22 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-/**
- * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
- */
 define( 'MOSAIC_CBIOS_CALCULATOR_VERSION', '1.0.0' );
 
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-mosaic_cbios_calculator-activator.php
- */
 function activate_mosaic_cbios_calculator() {
 	require_once plugin_dir_path( __FILE__ ) . 'includes/class-mosaic_cbios_calculator-activator.php';
 	Mosaic_cbios_calculator_Activator::activate();
 }
 
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-mosaic_cbios_calculator-deactivator.php
- */
 function deactivate_mosaic_cbios_calculator() {
 	require_once plugin_dir_path( __FILE__ ) . 'includes/class-mosaic_cbios_calculator-deactivator.php';
 	Mosaic_cbios_calculator_Deactivator::deactivate();
 }
 
-register_activation_hook( __FILE__, 'activate_mosaic_cbios_calculator' );
-register_deactivation_hook( __FILE__, 'deactivate_mosaic_cbios_calculator' );
-
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
 require plugin_dir_path( __FILE__ ) . 'includes/class-mosaic_cbios_calculator.php';
 
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
- */
-function run_mosaic_cbios_calculator() {
+register_activation_hook( __FILE__, 'activate_mosaic_cbios_calculator' );
+register_deactivation_hook( __FILE__, 'deactivate_mosaic_cbios_calculator' );
+register_deactivation_hook( __FILE__, 'deactivate_mosaic_cbios_calculator' );
 
-	$plugin = new Mosaic_cbios_calculator();
-	$plugin->run();
-
-}
-run_mosaic_cbios_calculator();
+$mscbios_calculator = new Mosaic_cbios_calculator();
